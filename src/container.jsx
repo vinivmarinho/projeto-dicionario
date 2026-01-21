53b41267d64ada76894a13214294f7d41dfad4c7
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-21 11:24:44 -0400

    chore: remove componente "container"

--- src/container.jsx
+++
@@ -1,7 +0,0 @@
-// Componente Container que renderiza uma div envolvendo seus children
-// children é uma prop especial do React que representa o conteúdo interno do componente
-export default function Container({ children }) {
-    return (
-        <div className="container">{children}</div>
-    )
-}
