536065a42f85b9408736cbc080c67626760d515a
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-21 11:55:23 -0400

    refactor: muda responsabilidade como container principal de "body" para a div "app"

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Cabecalho from "./header.jsx";
 function App() {
   const [fonte, setFonte] = useState("Arial"); // Estado que armazena a fonte atual e escolhida pelo usuário. ("fonte" => state atual. "setFonte" => Função que atualiza a fonte. "Arial" => Valor usado no primeiro render)
   return (
-    <div className={fonte}> {/*Div ganha a classe para que no css a font-family da página seja igual a fonte escolhida no menu "select"*/}
-      <Cabecalho fonte={fonte} setFonte={setFonte} />
-    </div>
+      <div className={`app ${fonte}`}> {/*Div ganha duas classes: a primeira(app) será usada como um container, a segunda(fonte) é para que no css a font-family da página seja igual a fonte escolhida no menu "select"*/}
+        <Cabecalho fonte={fonte} setFonte={setFonte} />
+      </div>
   );
 }
 
