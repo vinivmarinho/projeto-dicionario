99313d3cf8b36351e6e51e97d2202585cf0d21c8
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-26 11:52:42 -0400

    style: Cria section que guarda 2 divs, uma com a palavra e transcrição fonética, a outra guarda a classe gramatical e uma linha

--- src/components/word.jsx
+++ src/components/word.jsx
@@ -1,11 +1,17 @@
-import play from "../assets/icon-play.svg"
-import "../style/word.css"
+import play from "../assets/icon-play.svg";
+import "../style/word.css";
 export default function Palavra() {
-    return(
-        <div className="palavra">
-            <h1 id="palavra_pesquisada">Keyboard</h1>
-            <p id="transcricao_fonetica">/ˈkiːbɔːd/</p>
-            <img src={play}/>
-        </div>
-    )
+  return (
+    <section id="container_palavra">
+      <div id="cabecalho_palavra">
+        <h1 id="palavra_pesquisada">Keyboard</h1>
+        <p id="transcricao_fonetica">/ˈkiːbɔːd/</p>
+        <img src={play} id="play" />
+      </div>
+      <div id="container_classe_gramatical">
+        <p id="classe_gramatical">noun</p>
+        <div id="linha"></div>
+      </div>
+    </section>
+  );
 }
