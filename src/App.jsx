8e31a64d966e602373f77f2ead18468071b4ab2e
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-03-10 11:15:59 -0400

    refactor: Organiza as medias queries no final do código

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className={`app ${font}`}>
       {/*Div ganha duas classes: a primeira(app) será usada como um container, a segunda(fonte) será usada para controlar o estilo de fonte via CSS"*/}
-      <Header />
+      <Header font={font} setFont={setFont} />
       <main>
         <SearchBar />
         <Meaning />
